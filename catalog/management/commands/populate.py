50fac886c2bc8b80b9adf741c74b2192ee781555
youremail@email.com <you@example.com>
2021-08-14 01:05:01 +0200

    added populate

---
+++ catalog/management/commands/populate.py
@@ -0,0 +1,93 @@
+# Populate database
+# This file has to be placed within the
+# catalog/management/commands directory in your project.
+# If that directory doesn't exist, create it.
+# The name of the script is the name of the custom command,
+# that is, populate.py.
+#
+# execute python manage.py  populate
+#
+# use module Faker generator to generate data (https://zetcode.com/python/faker/)
+import os
+
+from django.core.management.base import BaseCommand
+from catalog.models import (Author, Book, Comment)
+from django.contrib.auth.models import User
+from faker import Faker
+from decimal import Decimal
+from library.settings import STATIC_PATH
+from PIL import Image, ImageDraw, ImageFont
+
+
+FONTDIR = "/usr/share/fonts/truetype/freefont/FreeMono.ttf"
+
+# The name of this class is not optional must be Command
+# otherwise manage.py will not process it properly
+#
+class Command(BaseCommand):
+    # helps and arguments shown when command python manage.py help populate
+    # is executed.
+    help = """populate database
+           """
+
+    # def add_arguments(self, parser):
+
+    # handle is another compulsory name, do not change it"
+    # handle function will be executed by 'manage populate'
+    def handle(self, *args, **kwargs):
+        self.NUMBERUSERS = 20
+        self.NUMBERBOOKS = 30
+        self.NUMBERAUTHORS = 5
+        self.MAXAUTHORSPERBOOK = 3
+        self.NUMBERCOMMENTS = self.NUMBERBOOKS * 5
+        self.MAXCOPIESSTOCK = 30
+        self.cleanDataBase()   # clean database
+        # The faker.Faker() creates and initializes a faker generator,
+        self.faker = Faker()
+        self.user()
+        self.author()
+        self.book()
+        self.comment()
+
+    def cleanDataBase(self):
+        # delete all models stored (clean table)
+        # in database
+        # remove pass and ADD CODE HERE
+        pass
+
+    def user(self):
+        " Insert users"
+        # remove pass and ADD CODE HERE
+        pass
+
+    def author(self):
+        " Insert authors"
+        # remove pass and ADD CODE HERE
+        pass
+
+    def cover(self, book):
+        """create fake cover image.
+           This function creates a very basic cover
+           that show (partially), 
+           the primary key, title and author name"""
+
+        img = Image.new('RGB', (200, 300), color=(73, 109, 137))
+        # your font directory may be different
+        fnt = ImageFont.truetype(
+            FONTDIR,
+            28, encoding="unic")
+        d = ImageDraw.Draw(img)
+        d.text((10, 100), "PK %05d" % book.id, font=fnt, fill=(255, 255, 0))
+        d.text((20, 150), book.title[:15], font=fnt, fill=(255, 255, 0))
+        d.text((20, 200), "By %s" % str(book.author.all()[0])[:15], font=fnt, fill=(255, 255, 0))
+        img.save(os.path.join(STATIC_PATH, book.path_to_cover_image))
+
+    def book(self):
+        " Insert books"
+        # remove pass and ADD CODE HERE
+        pass
+
+    def comment(self):
+        " Insert comments"
+        # remove pass and ADD CODE HERE
+        pass
