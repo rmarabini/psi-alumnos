613177c28854f9e182f2fca0cbcd351891f3a2c3
rmarabini <roberto.marabini.cnb@gmail.com>
2024-03-20 22:07:34 +0100

    Update test_models.py
    change 
    from .models import ChessGame,...
    by
    from models.models import Player, ChessGame, chessMove
    since this is more robust (does not force to place the test in the model directory)

--- server/models/test_models.py
+++ server/models/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .models import Player, ChessGame, ChessMove
+from models.models import Player, ChessGame, chessMove
 
 User = get_user_model()
 
