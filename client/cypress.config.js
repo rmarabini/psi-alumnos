89a8b8c8d5a05576ffac2436c9e19af268d78587
Your Name <you@example.com>
2025-03-27 15:20:53 +0100

    update

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 10000, // 10 seconds
     // append baseUrl to all URL used in the tests
     baseUrl: "http://localhost:5173",
     //specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
@@ -12,8 +13,10 @@ module.exports = defineConfig({
   // centralice some variables as username and password
   // use then in the test with "Cypress.env('username')"
   env: {
-    username: "user1@example.com",
-    username2: "user2@example.com",
-    password: "sacacorchos"
-  }
+    username: "y",
+    password: "y",
+    python: "/home/roberto/miniconda/envs/chess/bin/python",
+//    manage: "/home/roberto/Docencia/psi/2024-25/chesstournament/chesstournament_server/manage.py",
+    manage: "/home/roberto/Docencia/PSI/psi/2024_25/chesstournament/chesstournament_server/manage.py",
+  },
 });
