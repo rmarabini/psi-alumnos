5e91e32f62c8f61327f2cdf2066ae6c7e4e5e514
Your Name <you@example.com>
2025-03-27 09:32:38 +0100

    fix comma

--- server/chess_models/tests/test_models_game_tournament.py
+++ server/chess_models/tests/test_models_game_tournament.py
@@ -2,7 +2,7 @@ from django.test import TransactionTestCase, tag
 from chess_models.models import Tournament, Round, Game
 # from chess_models.models import (
 #    LichessAPIError, TournamentType, Scores)
-from chess_models.models import (getScores, getRanking
+from chess_models.models import (getScores, getRanking,
                                  getBlackWins)
 try:
     from chess_models.models import (getMediamBuchholz, getSonnebornBerger,
